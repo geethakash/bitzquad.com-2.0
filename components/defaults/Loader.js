9822071b03a8a9746637e8a5e04fe3108278c9a7
Akash Geethanjana <akashgeethanjana@gmail.com>
2022-06-16 16:10:28 +0530

    fix: remove loader circles

--- components/defaults/Loader.js
+++ components/defaults/Loader.js
@@ -2,21 +2,21 @@ import React, { useEffect } from 'react';
 import { Logo } from '../../constants/images';
 import { motion } from 'framer-motion';
 
-function Loader({setLoading}) {
+function Loader({ setLoading }) {
   return (
     <div className="loader ">
       <div className="w-full h-full flex justify-center items-center relative">
-        <div className="loader-circle circle-1"></div>
+        {/* <div className="loader-circle circle-1"></div>
         <div className="loader-circle circle-2"></div>
-        <div className="loader-circle circle-3"></div>
-        <div className="xl:w-2/12 w-8/12 aspect-square relative  rounded-full p-20">
+        <div className="loader-circle circle-3"></div> */}
+        <div className="xl:w-[400px] w-8/12 aspect-square relative  rounded-full p-20">
           <motion.img
             initial={{ scale: 0, opacity: 0 }}
             animate={{
               scale: 1,
               opacity: 1,
               transition: {
-                delay: 1,
+                // delay: 1,
                 duration: 1,
                 ease: 'easeInOut',
               },
