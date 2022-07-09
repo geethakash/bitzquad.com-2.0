aeedce0715176a54175afa9297507ac9ed8afcf2
hariwij <hariwijesinghe234@gmail.com>
2022-07-09 11:41:25 +0530

    feature : service infomation section

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 // local imports
 
-import { Collaborate, CustomerFeedback, Landing, LayoutMain, Loader, Testimonials } from '../components';
+import { Collaborate, CustomerFeedback, Landing, LayoutMain, Loader, Testimonials,ServiceCard } from '../components';
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -18,6 +18,16 @@ export default function Home() {
         <Landing loading={loading} />
         <Testimonials />
         <Collaborate/>
+        <ServiceCard 
+          title={'Information Systems'}
+          description={'We are passionate to build new digital products for a better future by collaborating with new clients. If you have an idea to create a new digital product for your business needs or any other questions, inquire us. Our team/specialists are always available to help you.'}
+          buttonText={'Learn More'}
+          color={'green'}/>
+        <ServiceCard 
+          title={'Business Process Re-engineering'}
+          description={'The practice of rethinking and redesigning the way work is done to better support an organization&quot;s mission and reduce costs.'}
+          buttonText={'Learn More'}
+          color={'purple'}/>
       </LayoutMain>
     </>
   );
