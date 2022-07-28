4dc81559ceb1e1acfb4aad218564db82aca13d1a
Akash Geethanjana <akashgeethanjana@gmail.com>
2022-07-28 20:14:54 +0530

    feat: our partners section ✨

--- constants/images.js
+++ constants/images.js
@@ -8,8 +8,7 @@ export { default as Testimonial_3 } from '../assets/images/testimonial-3.jpg';
 export { default as Testimonial_4 } from '../assets/images/testimonial-4.jpg';
 export { default as Testimonial_5 } from '../assets/images/testimonial-5.jpg';
 
-
-export { default as CollaborateImg }from '../assets/images/collaborate-img.png'
+export { default as CollaborateImg } from '../assets/images/collaborate-img.png';
 
 // cover images
 export { default as Landingsm } from '../assets/images/landing-sm.webp';
@@ -22,3 +21,7 @@ export { default as Shape4 } from '../assets/images/shapes/shape-4.png';
 export { default as Shape5 } from '../assets/images/shapes/shape-5.png';
 export { default as Shape6 } from '../assets/images/shapes/shape-6.png';
 export { default as Shape7 } from '../assets/images/shapes/shape-7.png';
+
+// partners images
+export { default as Partner_deinclusive } from '../assets/images/partners/partner-deinclusive.webp';
+export { default as Partner_wowhomies } from '../assets/images/partners/partner-wow-homies.webp';
