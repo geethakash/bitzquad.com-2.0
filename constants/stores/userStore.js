3194397c6d1826dd2e544a0a26cb36513128d4b7
hariwij <hariwijesinghe234@gmail.com>
2022-08-01 20:00:39 +0530

    remove all files

--- constants/stores/userStore.js
+++
@@ -1,8 +0,0 @@
-import create from "zustand";
-
-const useUserStore = create((set) => ({
-    user: {},
-    setUser: (newuser) => set({ user: newuser }),
-}));
-
-export default useUserStore;
--- pages/admin/blog/add.js
+++
@@ -1,4 +0,0 @@
-const Add = () => {
-    return <></>;
-};
-export default Add;
