90bd07f02bacba495fd7497b7e102190d4de773e
Akash Geethanjana <akashgeethanjana@gmail.com>
2022-08-01 13:06:43 +0530

    perf: add ease using framer motion

--- components/home/OurPartners.js
+++ components/home/OurPartners.js
@@ -33,7 +33,12 @@ function OurPartners() {
       <h3>Our partners</h3>
       <div className="partners_card_container">
         {partnerList.map((partner, index) => (
-          <div className="partners_card" key={index}>
+          <motion.div
+            className="partners_card"
+            key={index}
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.4, ease: [0.65, 0.05, 0.36, 1] }}
+          >
             <a href={partner.url} target="_blank" rel="noopener noreferrer">
               <img
                 className="partners_card_image"
@@ -42,7 +47,7 @@ function OurPartners() {
                 alt={partner.name}
               />
             </a>
-          </div>
+          </motion.div>
         ))}
       </div>
     </motion.div>
