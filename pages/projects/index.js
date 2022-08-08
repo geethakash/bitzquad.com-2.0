cfb1061481654d15bd86700de57364161b46342f
Akash Geethanjana <akashgeethanjana@gmail.com>
2022-08-08 10:50:26 +0530

    fix: remove fragment and add index as key

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -16,38 +16,37 @@ function index() {
         </div>
         <div className="pt-32 pb-52">
           {projects.map((project, index) => (
-            <>
+            <div
+              key={index}
+              className={`flex w-full  gap-x-10 ${
+                index % 2 == 1 ? 'flex-row-reverse' : ''
+              }`}
+            >
               <div
-                className={`flex w-full  gap-x-10 ${
-                  index % 2 == 1 ? 'flex-row-reverse' : ''
+                className={`flex  w-1/2 flex-col justify-center ${
+                  index % 2 == 1 ? '  text-left' : 'items-end text-right'
                 }`}
               >
-                <div
-                  className={`flex  w-1/2 flex-col justify-center ${
-                    index % 2 == 1 ? '  text-left' : 'items-end text-right'
-                  }`}
-                >
-                  <h3 className=" text-3xl font-bold tracking-widest first-letter:capitalize">
-                    {project.name}
-                  </h3>
-                  <p className="w-10/12 text-base">{project.description}</p>
-                </div>
-                <div className="relative aspect-[9/6] w-1/2">
-                  <Link href={`/${project.name}`}>
-                    <div className="absolute bottom-1/2 z-40 aspect-[8/7] w-full translate-y-[50%] overflow-hidden ">
-                      <div className="relative max-h-full w-full  bg-green-400"></div>
-                      <img
-                        src={project.image}
-                        data-scroll
-                        data-scroll-speed="0.75"
-                        className=" absolute top-[-100px]  aspect-[4/5] w-full object-cover"
-                        alt={project.name}
-                      />
-                    </div>
-                  </Link>
-                </div>
+                <h3 className=" text-3xl font-bold tracking-widest first-letter:capitalize">
+                  {project.name}
+                </h3>
+                <p className="w-10/12 text-base">{project.description}</p>
               </div>
-            </>
+              <div className="relative aspect-[9/6] w-1/2">
+                <Link href={`/${project.name}`}>
+                  <div className="absolute bottom-1/2 z-40 aspect-[8/7] w-full translate-y-[50%] overflow-hidden ">
+                    <div className="relative max-h-full w-full  bg-green-400"></div>
+                    <img
+                      src={project.image}
+                      data-scroll
+                      data-scroll-speed="0.75"
+                      className=" absolute top-[-100px]  aspect-[4/5] w-full object-cover"
+                      alt={project.name}
+                    />
+                  </div>
+                </Link>
+              </div>
+            </div>
           ))}
         </div>
       </div>
