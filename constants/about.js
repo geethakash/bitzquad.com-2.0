2f5abac3cfde9b9ecd9f2602f0f35f95aecb6b3b
hariwij <hariwijesinghe234@gmail.com>
2022-08-15 10:06:09 +0530

    about section

---
+++ constants/about.js
@@ -0,0 +1,29 @@
+import { About1, About2, About3 } from "./images";
+const about = [
+    {
+        t1: "We are a team of ",
+        t2: "Solutions",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis felis in neque mollis, quis sollicitudin tellus auctor. Vivamus et quam ut sem vehicula convallis. Nam consequat hendrerit congue.",
+        image: "https://images.news18.com/ibnlive/uploads/2022/01/office-2-16430115503x2.jpg?impolicy=website&width=510&height=356",
+        imgtxt: About1.src,
+    },
+    {
+        t1: "We elevate you to ",
+        t2: "another world.",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis felis in neque mollis, quis sollicitudin tellus auctor. Vivamus et quam ut sem vehicula convallis. Nam consequat hendrerit congue.",
+        image: "https://www.is2.de/wp-content/uploads/angular-kompetenzen-is2.jpg",
+        imgtxt: About2.src,
+    },
+    {
+        t1: "You may also be ",
+        t2: "Interested In...",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis felis in neque mollis, quis sollicitudin tellus auctor. Vivamus et quam ut sem vehicula convallis. Nam consequat hendrerit congue.",
+        image: "https://userneeds.com/wp-content/uploads/2020/06/U_Plus_Profile_user.jpg",
+        imgtxt: About3.src,
+    },
+];
+
+export default about;
