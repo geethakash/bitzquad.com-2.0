eec617f1b3135eab9f2f830671fbcf7caf390a5d
hariwij <hariwijesinghe234@gmail.com>
2022-08-30 19:07:09 +0530

    update img services

--- components/elements/Button.js
+++ components/elements/Button.js
@@ -9,6 +9,7 @@ function Button({ children, className, parentClassName, href }) {
                     whileHover={{ scale: 1.07 }}
                     whileTap={{ scale: 0.95 }}
                     transition={{ type: "spring", stiffness: 200 }}
+                    data-cursor-text="click"
                     className={`relative overflow-hidden rounded-full border-2 border-purple-700 px-5 py-3 text-xl sm:px-10 sm:py-4   ${className}`}
                 >
                     {/* <motion.div className="absolute bottom-0 left-0 top-0 right-0 h-full w-full bg-green-400 "></motion.div> */}
