6ee1b1c662950d74c756f63aab577c695931a558
hariwij <hariwijesinghe234@gmail.com>
2022-08-31 14:49:51 +0530

    apple icon

--- components/defaults/Meta.js
+++ components/defaults/Meta.js
@@ -6,6 +6,7 @@ export default function Meta(props) {
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <link rel="canonical" href={props.url}></link>
             <link href="/favicon.ico" rel="icon shortcut" type="image/x-icon"></link>
+            <link rel="apple-touch-icon" href="/favicon.ico"></link>
             <title>{props.title}</title>
             {/* Google */}
             <meta name="title" content={props.title} />
